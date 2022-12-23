7a1d1bb5c79b21ec8e55aa0e8b117863fa29006b
lperotti399 <103908232+lperotti399@users.noreply.github.com>
2022-12-23 01:19:25 -0500

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,32 @@
-function findAuthorById(authors, id) {}
+ function findAuthorById(authors, id) {
+    let output = authors.filter(author => author.id === id)
+    return output[0]
+ }
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+    let output = books.filter(book => book.id === id)
+    return output[0]
+ }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+    //get all checked out books
+    let checkedOutBooks = books.filter(book => book.borrows.some(bookId => bookId.returned===false))
+    //get all returned books
+    let returnedBooks = books.filter(book => book.borrows.every(bookId => bookId.returned===true))
+    //combine checked out books with returned books
+    let output = [checkedOutBooks,returnedBooks]
+    return output
+ }
 
-function getBorrowersForBook(book, accounts) {}
+//function getBorrowersForBook (book,accounts) {}
+function getBorrowersForBook(book, accounts) {
+  let result = book.borrows.map((borrows) => {
+    let account = accounts.find((account) =>
+          account.id === borrows.id      )
+    return {...borrows, ...account}
+  })
+  return result.slice(0,10)
+}
 
 module.exports = {
   findAuthorById,
